aa43e13e14d1b9c29f3c9459c397d3decc45afe2
Khushi <khushikri0801@gmail.com>
2026-09-02 19:04:23 +0530

    Time: 0 ms (100%), Space: 15.1 MB (96.84%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        // the key point is to identify the sorted half 
         int n = nums.size();
-        int ans = -1;
-        for(int i = 0; i < n ; i++ ){
-            if(nums[i] == target)
-             ans = i;
-        }
-        return ans ;
+        int low = 0, high = n-1;
 
+        while( low <= high){
+            int mid = low + ( high - low )/2;
+             if( nums[mid] == target)
+              return mid ;
+
+            if(nums[low] <= nums[mid]){ // left half sorted
+               if(nums[low] <= target && target <= nums[mid]){
+                high = mid - 1;
+
+               }
+               else
+                low = mid + 1;
+            }
+            else{
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else
+                 high = mid - 1 ;
+            }
+        }
+        return -1 ;
+        
         
     }
 };
