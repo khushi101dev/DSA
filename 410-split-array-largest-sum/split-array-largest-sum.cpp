9d37bd405c916807c5efd30b60f418bdbbdd88cf
Khushi <khushikri0801@gmail.com>
2026-09-22 17:19:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (97.25%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -25,17 +25,19 @@ bool isPossible(vector<int>& nums , int mid , int k){
         int n = nums.size();
         if(k > n)
          return -1;
-
+        
+        int ans = - 1;
          while(low <= high){
             int mid = low + (high - low)/2;
             if(isPossible(nums, mid , k)){
+                ans = mid;
                 high = mid -1 ;
             }
             else{
                 low = mid + 1;
             }
          }
-         return high+1;
+         return ans;
         
     }
 };
